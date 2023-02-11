d06e34973bf66b79cb1b0d6b38752e9bc41e911f
lazaronazareno <lazaronazareno@gmail.com>
2023-02-11 09:45:42 -0300

    targetDate changed, canvas confetti

--- src/components/countdown.js
+++ src/components/countdown.js
@@ -1,3 +1,4 @@
+import confetti from 'canvas-confetti'
 import React, { useState, useEffect, useContext } from 'react'
 import { ColorContext } from '../context/colorContexts'
 
@@ -5,11 +6,12 @@ const Countdown = () => {
   const { theme } = useContext(ColorContext)
 
   const [countdown, setCountdown] = useState(null)
+  const [isFree, setIsFree] = useState(false)
 
   useEffect(() => {
     const intervalId = setInterval(() => {
       const currentDate = new Date()
-      const targetDate = new Date(2023, 2, 1)
+      const targetDate = new Date(2023, 1, 11, 17, 0, 0)
       const diffTime = targetDate - currentDate
       const seconds = Math.floor((diffTime / 1000) % 60)
       const minutes = Math.floor((diffTime / 1000 / 60) % 60)
@@ -18,8 +20,19 @@ const Countdown = () => {
       setCountdown({ days, hours, minutes, seconds })
     }, 1000)
 
+    if (countdown !== null && countdown.days <= 0 &&
+      countdown.hours <= 0 && countdown.minutes &&
+      countdown.seconds <= 0) {
+      setIsFree(true)
+    }
     return () => clearInterval(intervalId)
   }, [])
+
+  useEffect(() => {
+    if (isFree) {
+      confetti()
+    }
+  }, [setIsFree])
 
   return (
     <div className='countdown' style={{ color: theme.details === '#EAC7C7' ? 'black' : theme.details, backgroundColor: theme.bg }}>
@@ -27,7 +40,13 @@ const Countdown = () => {
         ? (
             'Loading...'
           )
-        : (
+        : countdown !== null && countdown.days <= 0 &&
+        countdown.hours <= 0 && countdown.minutes &&
+        countdown.seconds <= 0
+          ? (
+          <h2 style={{ fontSize: '5rem', margin: '0' }}>Kasu es Libre!!!!</h2>
+            )
+          : (
           <>
             <div className='timer'>
               <div>
@@ -49,7 +68,7 @@ const Countdown = () => {
             </div>
             <h1>Para que Kasu sea libre...</h1>
           </>
-          )}
+            )}
     </div>
 
   )
